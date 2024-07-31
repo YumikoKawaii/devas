f1c1a98b43254401f45792af117ac2b6dce779ae
Misteln <vu.la@teko.vn>
2024-07-31 17:39:38 +0700

    more migration

--- application/migration/migration.up.sql
+++ application/migration/migration.up.sql
@@ -1,4 +1,4 @@
--- init conversation
+-- init conversations
 create table conversations
 (
     `id` BIGINT auto_increment primary key,
@@ -10,12 +10,25 @@ create table conversations
 ) engine = InnoDB default charset =utf8mb4 collate =utf8mb4_unicode_ci;
 
 -- init messages
-create table if not exists messages
+create table messages
 (
     `id` BIGINT auto_increment primary key,
-    `conversation_id` varchar(36),
+    `conversation_id` BIGINT,
     `sender` varchar(36),
     `content` text,
     `content_type` varchar(36),
     `action_time` datetime default current_timestamp
+) engine = InnoDB default charset =utf8mb4 collate =utf8mb4_unicode_ci;
+
+create table if not exists users
+(
+    `id` BIGINT auto_increment primary key,
+    `email` varchar(100),
+    `hash_password` varchar(100),
+    `username` varchar(100) unique,
+    # can be fetch first time from gravatar
+    `avatar` varchar(100),
+    `created_at` datetime default current_timestamp,
+    `updated_at` datetime default current_timestamp,
+    `is_active` tinyint(1)
 ) engine = InnoDB default charset =utf8mb4 collate =utf8mb4_unicode_ci;
